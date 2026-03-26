5bde89e51903283371f548dadea14e10ffa22c1b
Gabriela Kotseva <gkotseva98@gmail.com>
2026-03-26 18:12:14 +0200

    Example component structure and styling created

---
+++ src/components/Who.jsx
@@ -0,0 +1,22 @@
+export const Who = () => {
+  return (
+    <div className="flex justify-evenly">
+      <div>
+        <img src="./image.png" className="w-40 h-40 contain-content" />
+      </div>
+      <div className="max-w-150">
+        <h1 className="heading-text">Who am I?</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          voluptatem, officia nostrum doloribus voluptatibus ab, sequi dolorum
+          nam inventore impedit delectus, numquam repellendus magnam in itaque
+          totam repudiandae eveniet omnis. Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Voluptatem quisquam inventore ratione.
+          Provident delectus perferendis est totam soluta. Quibusdam similique
+          delectus commodi excepturi tempora eveniet consectetur inventore
+          laboriosam voluptatum mollitia.
+        </p>
+      </div>
+    </div>
+  );
+};
